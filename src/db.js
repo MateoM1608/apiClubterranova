b7c17ab273cefe3dd9d1355c084d48d70125d1b9
Mateo <madime-1997@hotmail.com>
2023-09-08 09:07:46 -0500

    redeploy

--- src/db.js
+++ src/db.js
@@ -18,7 +18,6 @@ const BodaModel = require('./models/Bodas')
 const sequelize = new Sequelize(`${DATABASE}`,`${DB_USER}`,`${DB_PASSWORD}`,{
   host: `${DB_HOST}`,
   dialect:'mysql',
-  
   dialectOptions: {
     connectTimeout:100000
   },
